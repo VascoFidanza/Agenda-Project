fad2171f8e8c20b1ae1e02316b0ec69a3e3bec3f
vascofidanza <vasco.fidanza@gmail.com>
2024-11-17 18:53:22 +0000

    feat: generating fake data; local_settings.py created; organizing views in a package without breaking the code

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -18,6 +23,12 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category, 
+        on_delete=models.SET_NULL,
+        blank = True,
+        null = True
+    )
+    owner = models.ForeignKey(
+        User, 
         on_delete=models.SET_NULL,
         blank = True,
         null = True
